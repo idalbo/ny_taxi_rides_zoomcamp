f897b93ca80f4f819b84bb5c025b294bb8f8ebc7
Igor Dal Bo <igor.dal.bo@factorypal.com>
2022-02-13 15:36:29 +0100

    dummy

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -7,6 +7,6 @@ select
     cast(PULocationID as integer) as  pickup_locationid,
     cast(DOLocationID as integer) as dropoff_locationid,
     cast(pickup_datetime as timestamp) as pickup_datetime,
-    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime
 from {{ source('staging','fhv_tripdata') }}
 where dispatching_base_num is not null and extract(year from pickup_datetime) = 2019
